91dd19aa62c3c2fe0ffd51bd8f63f760fe0fc7e2
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-07-05 15:04:00 -0500

    saving data in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,108 +1,76 @@
-//script for scheduler
-//create function to populate today's date with moment
 //create function to make schedule editable and save to local storage
 //color code blocks for current, past and future times
 
 var schedule = {};
 
-// var creatTask = function(taskText, taskTime, taskDate) {
-//     var 
-// }
-
 var loadSchedule = function () {
-    //var dateFormat = "ddd, MMM Do";
-    // var todaysDate = moment();
+    //populate today's date
     var today = moment().format("ddd, MMM Do");
     console.log(today);
-    // var today = moment(todaysDate.format('ddd, MMM Do'));
     $("#currentDay").replaceWith(today).addClass("time-block");
-    // today.text = todaysDate.format('ddd, MMM Do');
 
+    //debugger;
+    //pull data from localStorage to populate schedule
     schedule = JSON.parse(localStorage.getItem("schedule"));
 
 
     //if nothing in localStorage, create a new array to track schedule
     if (!schedule) {
         schedule = {
-            //time8: []
-            // time9: [],
-            // time10: []
-            // 11: [],
-            // 12: [],
-            // 1: [],
-            // 2: [],
-            // 3: [],
-            // 4: [],
-            // 5:[]
+            time8: [],
+            time9: []
         };   
     }
 
     $.each(schedule, function(list, arr) {
-        console.log(list, arr);
 
-        arr.forEach(function(schedule) {
-            setSchedule(schedule.text, schedule.id);
-        });
-        // else {
-        //     setSchedule();
-        // }
-});
+        // console.log(list, arr);
+        // list.forEach(function(schedule) {
+        var newBox = $("<p>").attr("id", "test").text(schedule.text);
+        $("#test").replaceWith(newBox);
+    });
+    //     var newBox = $("<div>").addClass("col-8 future description schedule-box");
+    //    $("#schedule-box").replaceWith(schedule.text).append(newBox);
+        // popSchedule.addClass("col-8 future description schedule-box");
+        //console.log(arr.text);
+     
 
+        // arr.forEach(setSchedule(schedule));
+        // });
+    //});
 };
 
+    //save data to localStorage
 var saveSchedule = function () {
     localStorage.setItem("schedule", JSON.stringify(schedule));
 };
 
-$(".schedule-box").on("click", function () {
-    //event.preventDefault();
+//when schedule box is clicked, it becomes editable
+$("#schedule-box").on("click", function () {
+    //debugger;
+    event.preventDefault();
 
-    var text = $(this)
-        .text()
-        .trim();
+    var text = $(this).text().trim();
     console.log(text);
 
-    var textInput = $("<textarea>").addClass("col-8 future description").val(text);
+    var textInput = $("<textarea>").val(text).addClass("col-8 future description schedule-box");
 
     $(this).replaceWith(textInput)
     //textInput.trigger("focus");
  
 });
 
-// $(".schedule").on("blur", "textarea", function () {
+//saved data populates schedule
+// var setSchedule = function(scheduleText) {
+//     $("#schedule-box").replaceWith(scheduleText).addClass("col-8 future description schedule-box");
+//        // var id = $("div").closest(".row").attr("id").replace("-", "");
 
-//     //get the textarea's current value/text
-//     var text = $(this).val().trim();
-//     // console.log(text);
-
-//     //get the parent div's id attribute
-//     var time = $(this).closest(".row").attr("id").replace("time-", "");
-
-//     //get the tasks's position in the list of other li elements
-//     var index = $(this).closest("#time").index();
-
-//     schedule[time][index].text = text;
+//     // schedule.id.push({
+//     //     text: scheduleBoxText
+//     //});
+//     console.log(schedule);
 //     saveSchedule();
-
-//     //     //recreate p element
-//     var taskP = $("<p>").addClass("m-1").text(text);
-
-//     //     //replace textarea with a p element
-//     $(this).replaceWith(taskP);
-// });
-
-var setSchedule = function() {
-    var scheduleBoxText = $("textarea").val();
-    console.log(scheduleBoxText);
-
-    var id = $("div").closest(".row").attr("id").replace("-", "");
-
-    schedule.id.push({
-        text: scheduleBoxText
-    });
-    console.log(schedule);
-    saveSchedule();
-};
+// };
 
 // var setSchedule = function(scheduleText, scheduleId) {
 //     var scheduleDiv = $("<div>").addClass("col-8 future description schedule-box").attr("id", scheduleId).text(scheduleText);
@@ -117,40 +85,21 @@ var setSchedule = function() {
 
 // };
 
+//save button is clicked
 $(".saveBtn").click(function() {
-//     setSchedule();
-// });
-
-// loadSchedule();
-    //get the textarea's current value/text
-    //debugger;
+    debugger;
+    console.log(schedule);
     var text = $("textarea").val();
-    // console.log(text);
     console.log(text);
-
-    //var scheduleText = [];
     //get the parent div's id attribute
-    var id = $("div").closest(".row").attr("id").replace("-", "");
+    var id = $("div").closest(".row").attr("id");
     console.log(id);
-    //get the tasks's position in the list of other li elements
-    //var index = $("div").closest(".time").index();
-//console.log(index);
-    schedule.text = text;
-    // saveSchedule();
 
-    schedule.id.push({
-        text: text,
-        id: id
+    schedule[id].push({
+        text: text
     });
 
-        //recreate p element
-    var scheduleDiv = $("<div>").addClass("col-8 future description schedule-box").text(text);
-
-        //replace textarea with a p element
-$("textarea").replaceWith(scheduleDiv);
-    saveSchedule();
-    // console.log(schedule[id].text);
-
+   saveSchedule();
 });
 
 loadSchedule();

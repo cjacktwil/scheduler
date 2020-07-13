e9b049c69c5a74643400d02f0e03d0b3800e6768
Tracey Jackson <62776009+cjacktwil@users.noreply.github.com>
2020-07-12 22:48:19 -0500

    reworked code to simplify - now saves and retrieves from local storage, color-codes time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,327 +1,205 @@
-$(document).ready(function() {
+var schedule;
+var time = ["08", "09", "10", "11", "12", "13", "14", "15", "16", "17"];
 
-    var savedSchedule = [];
+var containerEl = $(".container");
 
-    var scheduleEightAm = $("#scheduleEight");
-    var scheduleNineAm = $("#scheduleNine");
+var auditTime = function () {
 
-    var saveSchedule = function() {
-        localStorage.setItem("savedSchedule", JSON.stringify(savedSchedule));
-    };
+    todayTime = moment().format("HH");
 
-    var loadSchedule = function() {
-         //populate today's date
+    //iterate through time array to determine if schedule time is past, present or future
+    for (i = 0; i < time.length; i++) {
+
+        scheduleTime = parseInt(time[i]);
+        todayTimeParse = parseInt(todayTime);
+
+        //if time on schedule is in the future
+        if (scheduleTime > todayTimeParse) {
+            console.log(scheduleTime);
+
+            if (scheduleTime === 08) {
+                $("#scheduleEight").addClass("future")
+            }
+            if (scheduleTime === 09) {
+                $("#scheduleNine").addCLass("future")
+            }
+            if (scheduleTime === 10) {
+                $("#scheduleTen").addCLass("future")
+            }
+            if (scheduleTime === 11) {
+                $("#scheduleEleven").addClass("future")
+            }
+            if (scheduleTime === 12) {
+                $("#scheduleTwelve").addClass("future")
+            }
+            if (scheduleTime === 13) {
+                $("#scheduleOne").addClass("future")
+            }
+            if (scheduleTime === 14) {
+                $("#scheduleTwo").addClass("future")
+            }
+            if (scheduleTime === 15) {
+                $("#scheduleThree").addClass("future")
+            }
+            if (scheduleTime === 16) {
+                $("#scheduleFour").addClass("future")
+            }
+            if (scheduleTime === 17) {
+                $("#scheduleFive").addClass("future")
+            }
+        }
+
+        //if schedule time is in the past
+        else if (scheduleTime < todayTimeParse) {
+
+            if (scheduleTime === 08) {
+                $("#scheduleEight").addClass("past")
+            }
+            if (scheduleTime === 09) {
+                $("#scheduleNine").addClass("past")
+            }
+            if (scheduleTime === 10) {
+                $("#scheduleTen").addClass("past")
+            }
+            if (scheduleTime === 11) {
+                $("#scheduleEleven").addClass("past")
+            }
+            if (scheduleTime === 12) {
+                $("#scheduleTwelve").addClass("past")
+            }
+            if (scheduleTime === 13) {
+                $("#scheduleOne").addClass("past")
+            }
+            if (scheduleTime === 14) {
+                $("#scheduleTwo").addClass("past")
+            }
+            if (scheduleTime === 15) {
+                $("#scheduleThree").addClass("past")
+            }
+            if (scheduleTime === 16) {
+                $("#scheduleFour").addClass("past")
+            }
+            if (scheduleTime === 17) {
+                $("#scheduleFive").addClass("past")
+            }
+        }
+
+        //if schedule time is present time
+        else {
+            if (scheduleTime === 08) {
+                $("#scheduleEight").addClass("present")
+            }
+            if (scheduleTime === 09) {
+                $("#scheduleNine").addClass("present")
+            }
+            if (scheduleTime === 10) {
+                $("#scheduleTen").addClass("present")
+            }
+            if (scheduleTime === 11) {
+                $("#scheduleEleven").addClass("present")
+            }
+            if (scheduleTime === 12) {
+                $("#scheduleTwelve").addClass("present")
+            }
+            if (scheduleTime === 13) {
+                $("#scheduleOne").addClass("present")
+            }
+            if (scheduleTime === 14) {
+                $("#scheduleTwo").addClass("present")
+            }
+            if (scheduleTime === 15) {
+                $("#scheduleThree").addClass("present")
+            }
+            if (scheduleTime === 16) {
+                $("#scheduleFour").addClass("present")
+            }
+            if (scheduleTime === 17) {
+                $("#scheduleFive").addClass("present")
+            }
+        }
+    }
+};
+
+auditTime();
+
+
+var loadSchedule = function () {
+    //populate today's date
     var today = moment().format("ddd, MMM Do");
-    console.log(today);
+
     $("#currentDay").replaceWith(today).addClass("time-block");
 
-        //pull data from localStorage to populate schedule
-    savedSchedule = JSON.parse(localStorage.getItem("savedSchedule"));
-    textEight = savedSchedule[0];
-    console.log(textEight);
-   scheduleEightAm = textEight;
-   console.log(scheduleEightAm);
-    
-   
-    //if nothing in localStorage, create a new array to track schedule
-    if (!savedSchedule) {
-        savedSchedule = [];   
-    }
-    // else {
-    //     scheduleEightAm.val(savedSchedule[0]);
-    //     scheduleNineAm = savedSchedule[1];
-    // };
-    saveSchedule();
-    };
+    //Load text from local storage
+    savedScheduleEight = JSON.parse(localStorage.getItem("saveEight"));
+    var scheduleEightEl = $("#scheduleEight")
+    scheduleEightEl.append(savedScheduleEight);
+    var timeBlockEight = "08"
 
-    // when schedule box is clicked, it becomes editable
+    savedScheduleNine = JSON.parse(localStorage.getItem("saveNine"));
+    var scheduleNineEl = $("#scheduleNine")
+    scheduleNineEl.append(savedScheduleNine);
+    var timeBlockNine = "09"
+
+    savedScheduleTen = JSON.parse(localStorage.getItem("saveTen"));
+    var scheduleTenEl = $("#scheduleTen")
+    scheduleTenEl.append(savedScheduleTen);
+    timeBlockTen = "10"
+
+    savedScheduleEleven = JSON.parse(localStorage.getItem("saveEleven"));
+    var scheduleElevenEl = $("#scheduleEleven")
+    scheduleElevenEl.append(savedScheduleEleven);
+
+    savedScheduleTwelve = JSON.parse(localStorage.getItem("saveTwelve"));
+    var scheduleTwelveEl = $("#scheduleTwelve")
+    scheduleTwelveEl.append(savedScheduleTwelve);
+
+    savedScheduleOne = JSON.parse(localStorage.getItem("saveOne"));
+    var scheduleOneEl = $("#scheduleOne")
+    scheduleOneEl.append(savedScheduleOne);
+
+    savedScheduleTwo = JSON.parse(localStorage.getItem("saveTwo"));
+    var scheduleTwoEl = $("#scheduleTwo")
+    scheduleTwoEl.append(savedScheduleTwo);
+
+    savedScheduleThree = JSON.parse(localStorage.getItem("saveThree"));
+    var scheduleThreeEl = $("#scheduleThree")
+    scheduleThreeEl.append(savedScheduleThree);
+
+    savedScheduleFour = JSON.parse(localStorage.getItem("saveFour"));
+    var scheduleFourEl = $("#scheduleFour")
+    scheduleFourEl.append(savedScheduleFour);
+
+    savedScheduleFive = JSON.parse(localStorage.getItem("saveFive"));
+    var scheduleFiveEl = $("#scheduleFive")
+    scheduleFiveEl.append(savedScheduleFive);
+
+
+};
+
+loadSchedule();
+
+
+//turn schedule box to text area
 $(".schedule-box").on("click", function () {
-    //debugger;
     event.preventDefault();
 
     var text = $(this).text().trim();
-    console.log(text);
+    // console.log(text);
 
-    var textInput = $("<textarea>").val(text).addClass("col-8 future description schedule-box");
+    var textInput = $("<textarea>").val(text).addClass("col-8 description schedule-box");
 
     $(this).replaceWith(textInput)
-//     //textInput.trigger("focus");
- 
+
 });
 
-//save button is clicked
-$("#saveEight").click(function() {
+//save information to local storage when click save button 
+$(".saveBtn").on("click", function () {
+    var saveButton = event.currentTarget;
+    var saveButtonId = $(saveButton).attr("id");
+    var text = $(saveButton).prev().val();
+    schedule = text;
+    id = saveButtonId;
 
- var id = $(this).closest(".row").attr("id");
- console.log(id);
- var text = $(this).prev().val();
- console.log(text);
-
-savedSchedule[0] = text;
-
-   saveSchedule();
+    localStorage.setItem(id, JSON.stringify(schedule));
 });
 
-$("#saveNine").click(function() {
-
-var id = $(this).closest(".row").attr("id");
- console.log(id);
- var text = $(this).prev().val();
- console.log(text);
-
- savedSchedule[1] = text;
-
-saveSchedule();
-});
-
-$("#saveTen").click(function() {
-
-   var id = $(this).closest(".row").attr("id");
- console.log(id);
- var text = $(this).prev().val();
- console.log(text);
-   
-savedSchedule[2] = text;
-   
-   saveSchedule();
-   });
-
-   $("#saveEleven").click(function() {
-
-    var id = $(this).closest(".row").attr("id");
- console.log(id);
- var text = $(this).prev().val();
- console.log(text);
-   
-    savedSchedule[3] = text;
-   
-   saveSchedule();
-   });
-
-   $("#saveTwelve").click(function() {
-
-    var id = $(this).closest(".row").attr("id");
-    console.log(id);
-    var text = $(this).prev().val();
-    console.log(text);
-   
-    savedSchedule[4] = text;
-   
-   saveSchedule();
-   });
-
-   $("#saveOne").click(function() {
-
-    var id = $(this).closest(".row").attr("id");
-    console.log(id);
-    var text = $(this).prev().val();
-    console.log(text);
-   
-    savedSchedule[5] = text;
-   
-   saveSchedule();
-   });
-
-   $("#saveTwo").click(function() {
-
-    var id = $(this).closest(".row").attr("id");
- console.log(id);
- var text = $(this).prev().val();
- console.log(text);
-   
-    savedSchedule[6] = text;
-   
-   saveSchedule();
-   });
-
-   $("#saveThree").click(function() {
-
-    var id = $(this).closest(".row").attr("id");
-    console.log(id);
-    var text = $(this).prev().val();
-    console.log(text);
-   
-    savedSchedule[7] = text;
-   
-   saveSchedule();
-   });
-
-   $("#saveFour").click(function() {
-
-    var id = $(this).closest(".row").attr("id");
- console.log(id);
- var text = $(this).prev().val();
- console.log(text);
-   
-    savedSchedule[8] = text;
-   
-   saveSchedule();
-   });
-
-   $("#saveFive").click(function() {
-
-    var id = $(this).closest(".row").attr("id");
-    console.log(id);
-    var text = $(this).prev().val();
-    console.log(text);
-   
-    savedSchedule[9] = text;
-   
-   saveSchedule();
-   });
-    //     var scheduleEight = $(this).prev();
-    //     console.log(scheduleEight);
-    //     var scheduleEightText = scheduleEight.value;
-    //     console.log(scheduleEightText);
-
-    //     if ($("#saveEight")) {
-    //         savedSchedule[0] = scheduleEightAm.value;
-    //     }
-    //     else if ($("saveNine")) {
-    //         savedSchedule[1].text = scheduleNineAm;
-    //     }
-
-    // console.log(savedSchedule);
-        // debugger;
-        // var itemId = $(this).attr("id").replace("save", "");
-        // console.log(itemId);
-        // var scheduleItemId = ("schedule" + itemId);
-        // console.log(scheduleItemId);
-        // var scheduleItemText = $("textarea").attr("id", scheduleItemId).val().trim();
-        // console.log(scheduleItemText);
-
-        // savedSchedule[scheduleItemId].push ({
-        //     text: scheduleItemText
-        //     });
-
-        // $(scheduleItemId.text).replaceWith(scheduleItemText);
-    //     saveSchedule();
-
-    // });
-
-loadSchedule();
-});
-//     var savedEightAmSchedule = localStorage.getItem("eightAmSchedule") || "";
-
-//     var eightAmSchedule = $("#scheduleEight");
-//     var nineAmSchedule = $("#scheduleNine");
-
-//     var eightAmButton = $("#saveEight");
-//     var nineAmButton = $("#saveNine");
-
-//     eightAmSchedule.val(savedEightAmSchedule);
-
-//     function saveTextEight() {
-//         eightAmInput = eightAmSchedule.val();
-//         console.log(eightAmInput);
-//         localStorage.setItem("eightAmSchedule", JSON.stringify(eightAmInput));
-//     }
-
-//     eightAmButton.click(saveTextEight);
-
-// });
-
-// var schedule = {};
-
-// // var timeSlotEight = $("#schedule-8");
-// // var timeSlotNine = $("schedule-9").val();
-
-// var loadSchedule = function () {
-//     //populate today's date
-//     var today = moment().format("ddd, MMM Do");
-//     console.log(today);
-//     $("#currentDay").replaceWith(today).addClass("time-block");
-
-//     //debugger;
-//     //pull data from localStorage to populate schedule
-//     schedule = JSON.parse(localStorage.getItem("schedule"));
-
-
-//     //if nothing in localStorage, create a new array to track schedule
-//     if (!schedule) {
-//         schedule = 
-//             timeSlotEight = [],
-//             timeSlotNine = []  
-//     };   
-//     };
-
-// //     $.each(schedule, function(list, arr) {
-
-// //         console.log(list, arr);
-// //         arr.forEach(function(schedule) {
-// //             console.log(schedule.text, schedule.id, list);
-// //             setSchedule(schedule.text, schedule.id, list);
-// //     });
-// //  });
-// // };
-
-// var setSchedule = function(scheduleText, scheduleId, scheduleList) {
-//     //debugger;
-//     console.log(scheduleText, scheduleId, scheduleList);
-//     var id = $("div").closest(".row").attr("id");
-//     var index = 0;
-//     var scheduleBox = $("<div>").addClass("col-8 future description schedule-box").text(schedule[id].text);
-
-//     console.log(scheduleBox);
-//     $("#test").replaceWith(scheduleBox);
-// };
-
-//     //save data to localStorage
-// var saveSchedule = function () {
-//     localStorage.setItem("schedule", JSON.stringify(schedule));
-// };
-
-// // when schedule box is clicked, it becomes editable
-// $(".schedule-box").on("click", "p", function () {
-//     //debugger;
-//     event.preventDefault();
-
-//     var text = $(this).text().trim();
-//     console.log(text);
-
-//     var textInput = $("<textarea>").val(text).addClass("col-8 future description schedule-box");
-
-//     $(this).replaceWith(textInput)
-// //     //textInput.trigger("focus");
- 
-// });
-// //save button is clicked
-// $(".saveBtn").click(function() {
-//     // $("save8").click(function() {
-//     //     scheduleAtEight = timeSlotEight.val().trim();
-//     //     console.log(scheduleAtEight);
-//     // })
-//     // timeSlotEight = $("#scheduleEight").val().trim();
-//     // console.log(timeSlotEight);
-//     // localStorage.setItem("timeSlotEight", JSON.stringify(timeSlotEight));
-//     // var row = 
-//     // debugger;
-//     // var row = $(this).closest("div").attr(".row");
-//     // console.log(row);
-
-//     //get the parent div's id attribute
-//     var id = $(event.target).closest(".row").attr("id");
-//     console.log(id);
-//     // var textAreaText = $(id).find("textarea").val();
-//     var textIdNumber = id.replace("time", "");
-//     var textId = ("schedule" + textIdNumber);
-//     console.log(textId);
-//     // var pId = ('"' + '#p' + textIdNumber + '"');
-//     // console.log(pId);
-//     // console.log(textAreaText);
-//     var text = $(textId).val();
-    
-// //     // iv" id).find("p").val();
-//     console.log(text);
-   
-    
-// //     // schedule[id].push({
-// //     //     text: text
-// //     // });
-
-// // //    saveSchedule();
-// });
-
-// loadSchedule();
-
-// });
